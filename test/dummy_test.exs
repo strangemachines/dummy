a5f6a8718967acbcbf6840acee3993799fe60a25
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-02-27 03:12:28 +0000

    tests/dummy: add test for dummy with more methods

--- test/dummy_test.exs
+++ test/dummy_test.exs
@@ -10,6 +10,13 @@ defmodule DummyTest do
     end
   end
 
+  test "the dummy macro with more mocks" do
+    dummy IO, ["puts", "chardata_to_string"] do
+      assert IO.puts("hello") == "hello"
+      assert IO.chardata_to_string([0x0061]) == [0x0061]
+    end
+  end
+
   test "the dummy macro with options" do
     dummy IO, ["puts"], passthrough: false do
       assert IO.puts("hello") == "hello"
