fa81ad2da3d4b0d39a3dc29906c3fb00a18ef7af
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-02-27 03:00:52 +0000

    feat/dummy: add apply_options function

--- lib/dummy.ex
+++ lib/dummy.ex
@@ -3,15 +3,22 @@ defmodule Dummy do
   Documentation for Dummy.
   """
 
+  defp apply_options(module, options) do
+    if options[:passthrough] == false do
+      :meck.new(module)
+    else
+      :meck.new(module, [:passthrough])
+    end
+  end
 
   @doc """
   Mocks methods of a single module. By defualt mocked methods return their first argument by default and non-mocked methods are passed through.
   """
-  defmacro dummy(module_name, methods_list, do: test) do
+  defmacro dummy(module_name, methods_list, options \\ [], do: test) do
     quote do
       module = unquote(module_name)
       methods = unquote(methods_list)
-      :meck.new(module, [:passthrough])
+      apply_options(module, unquote(options))
 
       for method <- methods,
           do: :meck.expect(module, String.to_atom(method), fn x -> x end)
