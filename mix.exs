3743825e31dca0cf48492f3480d5870d16e9319d
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-02-27 03:48:01 +0000

    chore/mix: prepare for publishing

--- mix.exs
+++ mix.exs
@@ -4,9 +4,11 @@ defmodule Dummy.MixProject do
   def project do
     [
       app: :dummy,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: "1.0.0",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,6 +23,21 @@ defmodule Dummy.MixProject do
     [
       {:credo, "~> 0.9.1", only: [:dev, :test], runtime: false},
       {:meck, "~> 0.8.13"}
+    ]
+  end
+
+  defp description do
+    "Elixir mocking that makes sense. Dummy relies on meck and exposes a
+      simpler way to mock methods than mock"
+  end
+
+  defp package do
+    [
+      name: :dummy,
+      files: ~w(mix.exs lib .formatter.exs README.md LICENSE),
+      maintainers: ["Jacopo Cascioli"],
+      licenses: ["MPL 2.0"],
+      links: %{"GitHub" => "https://github.com/Vesuvium/dummy"}
     ]
   end
 end
