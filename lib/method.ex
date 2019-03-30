e25dfa34a7225575c526cf8f6f8cc283da2db3c4
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-03-30 11:01:39 +0100

    refactor/method: split replace into private functions

--- lib/method.ex
+++ lib/method.ex
@@ -3,27 +3,35 @@ defmodule Dummy.Method do
   Handles replacing methods
   """
 
+  defp from_tuple(module, method) do
+    function_name = String.to_atom(elem(method, 0))
+
+    if is_function(elem(method, 1)) do
+      :meck.expect(module, function_name, elem(method, 1))
+    else
+      :meck.expect(module, function_name, fn _x -> elem(method, 1) end)
+    end
+  end
+
+  defp from_string(module, method) do
+    shards = String.split(method, "/")
+
+    if Enum.count(shards) == 2 do
+      :meck.expect(module, String.to_atom(Enum.at(shards, 0)), fn x, y -> [x, y] end)
+    else
+      :meck.expect(module, String.to_atom(method), fn x -> x end)
+    end
+  end
+
   @doc """
   Replaces a method with a mock, according to how the mock was defined:
   either with "function" or {"function", value} or {"function", fn}
   """
   def replace(module, method) do
     if is_tuple(method) do
-      function_name = String.to_atom(elem(method, 0))
-
-      if is_function(elem(method, 1)) do
-        :meck.expect(module, function_name, elem(method, 1))
-      else
-        :meck.expect(module, function_name, fn _x -> elem(method, 1) end)
-      end
+      from_tuple(module, method)
     else
-      shards = String.split(method, "/")
-
-      if Enum.count(shards) == 2 do
-        :meck.expect(module, String.to_atom(Enum.at(shards, 0)), fn x, y -> [x, y] end)
-      else
-        :meck.expect(module, String.to_atom(method), fn x -> x end)
-      end
+      from_string(module, method)
     end
   end
 end
