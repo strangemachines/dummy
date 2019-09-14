98e4ef07cc549e31365cb5c57e0df9f021402a45
Jacopo Cascioli <jacopo@jacopocascioli.com>
2019-09-15 01:46:27 +0200

    feat/method: replace_from_string supports specifying arities of 1

--- test/unit/method_test.exs
+++ test/unit/method_test.exs
@@ -12,6 +12,11 @@ defmodule DummyTest.Method do
     assert Time.utc_now() == "utc_now"
   end
 
+  test "replacing a method specifying an arity of 1" do
+    Method.replace(File, "read/1")
+    assert File.read("hello") == "hello"
+  end
+
   test "replacing a method with an arity of 2" do
     Method.replace(File, "open/2")
     assert File.open("hello", "world") == ["hello", "world"]
