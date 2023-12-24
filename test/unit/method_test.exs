2077cd5ad409cf2d74f14adcc2bb356311cf8b5f
nomorepanic <nomorepanic@strangemachines.dev>
2023-12-24 19:53:41 +0100

    refactor/method: replace uses pattern matching

--- test/unit/method_test.exs
+++ test/unit/method_test.exs
@@ -2,42 +2,42 @@ defmodule DummyTest.Method do
   use ExUnit.Case
   alias Dummy.Method
 
-  test "replacing a method" do
+  test "replace/2 with a string" do
     Method.replace(File, "read")
     assert File.read("hello") == "hello"
   end
 
-  test "replacing a method with an arity of 0" do
+  test "replace/2 with function_name/0" do
     Method.replace(Time, "utc_now/0")
     assert Time.utc_now() == "utc_now"
   end
 
-  test "replacing a method specifying an arity of 1" do
+  test "replace/2 with function_name/1" do
     Method.replace(File, "read/1")
     assert File.read("hello") == "hello"
   end
 
-  test "replacing a method with an arity of 2" do
+  test "replace/2 with function_name/2" do
     Method.replace(File, "open/2")
     assert File.open("hello", "world") == ["hello", "world"]
   end
 
-  test "replacing a method with a specified arity of 3" do
+  test "replace/2 with function_name/3" do
     Method.replace(File, "open/3")
     assert File.open("hello", "world", "!") == ["hello", "world", "!"]
   end
 
-  test "replacing a function with an arity of 4" do
+  test "replace/2 with function_name/4" do
     Method.replace(String, "replace/4")
     assert String.replace(1, 2, 3, 4) == [1, 2, 3, 4]
   end
 
-  test "replacing a method with a specified value" do
+  test "replace/2 with a value" do
     Method.replace(File, {"read", "world"})
     assert File.read("hello") == "world"
   end
 
-  test "replacing a method with a specified function" do
+  test "replace/2 with a function" do
     Method.replace(File, {"read", fn _a -> "hello world" end})
     assert File.read("hello") == "hello world"
   end
