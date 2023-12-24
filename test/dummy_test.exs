24866cad2154949c78b2d99bbfd2e96caaeef885
nomorepanic <nomorepanic@strangemachines.dev>
2023-12-24 19:53:41 +0100

    feat/dummy: add apply_options function

--- test/dummy_test.exs
+++ test/dummy_test.exs
@@ -9,4 +9,16 @@ defmodule DummyTest do
       assert IO.chardata_to_string([0x0061]) == "a"
     end
   end
+
+  test "the dummy macro with options" do
+    dummy IO, ["puts"], passthrough: false do
+      assert IO.puts("hello") == "hello"
+
+      try do
+        IO.chardata_to_string([0x0061]) == "a"
+      rescue
+        _error in UndefinedFunctionError -> nil
+      end
+    end
+  end
 end
