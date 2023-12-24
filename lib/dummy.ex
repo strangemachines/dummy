62c3cd152135499abbc28b6d054d4e917d143391
nomorepanic <nomorepanic@strangemachines.dev>
2023-12-24 19:53:41 +0100

    feat/dummy: allow specifying only return values instead of whole replacement methods

--- lib/dummy.ex
+++ lib/dummy.ex
@@ -13,7 +13,13 @@ defmodule Dummy do
 
   def replace_method(module, method) do
     if is_tuple(method) do
-      :meck.expect(module, String.to_atom(elem(method, 0)), elem(method, 1))
+      function_name = String.to_atom(elem(method, 0))
+
+      if is_function(elem(method, 1)) do
+        :meck.expect(module, function_name, elem(method, 1))
+      else
+        :meck.expect(module, function_name, fn _x -> elem(method, 1) end)
+      end
     else
       :meck.expect(module, String.to_atom(method), fn x -> x end)
     end
