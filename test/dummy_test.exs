351ae3d55c8aed7d161fa3b179df7a57330bff95
nomorepanic <nomorepanic@strangemachines.dev>
2023-12-24 19:53:41 +0100

    feat/dummy: add dummy macro

--- test/dummy_test.exs
+++ test/dummy_test.exs
@@ -1,8 +1,12 @@
 defmodule DummyTest do
   use ExUnit.Case
   doctest Dummy
+  import Dummy
 
-  test "greets the world" do
-    assert Dummy.hello() == :world
+  test "the dummy macro" do
+    dummy IO, ["puts"] do
+      assert IO.puts("hello") == "hello"
+      assert IO.chardata_to_string([0x0061]) == "a"
+    end
   end
 end
